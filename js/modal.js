e1e074df070c1a42f336859f8dbfdd218c684f75
Ivan Lomov <>
2020-11-01 19:50:24 +0300

    Внес правки

--- js/modal.js
+++ js/modal.js
@@ -10,9 +10,13 @@ const storageEmail = localStorage.getItem("userEmailValue");
 
 
 link.addEventListener("click", function (evt) {
-evt.preventDefault ();
+evt.preventDefault();
 popup.classList.add("modal-show");
-userName.focus ();
+form.classList.remove("modal-error");
+userEmail.classList.remove("input-error");
+userText.classList.remove("input-error");
+userName.classList.remove("input-error");
+userName.focus();
 if (storageName && storageEmail) {
     userName.value = storageName;
     userEmail.value = storageEmail;
@@ -24,14 +28,24 @@ if (storageName && storageEmail) {
 });
 
 closePopup.addEventListener("click", function (evt) {
-evt.preventDefault ();
+evt.preventDefault();
 popup.classList.remove("modal-show");
 });
+
 
 form.addEventListener("submit", function (evt) {
     if (!userName.value) {
     evt.preventDefault();
     userName.classList.add("input-error");
+    form.classList.add("modal-error");
+} if (!userEmail.value) {
+    evt.preventDefault();
+    userEmail.classList.add("input-error");
+    form.classList.add("modal-error");
+} if (!userText.value) {
+    evt.preventDefault();
+    userText.classList.add("input-error");
+    form.classList.add("modal-error");
 } else {
     localStorage.setItem("userNameValue", userName.value);
     localStorage.setItem("userEmailValue", userEmail.value);
@@ -40,6 +54,14 @@ form.addEventListener("submit", function (evt) {
 
 userName.addEventListener("click", function () {
     userName.classList.remove("input-error");
+});
+
+userEmail.addEventListener("click", function () {
+    userEmail.classList.remove("input-error");
+});
+
+userText.addEventListener("click", function () {
+    userText.classList.remove("input-error");
 });
 
 window.addEventListener("keydown", function (evt) {
